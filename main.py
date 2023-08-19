f0d94374038a3ecc6a338237b12ceb6677486c39
muhammadredin <115381364+muhammadredin@users.noreply.github.com>
2023-08-19 10:00:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ def app_sst():
         with open(audio_path, "wb") as f:
             f.write(audio_data)
 
-        encoded_audio_data = base64.b64encode(audio_data).decode()
+        encoded_audio_data = base64.b64encode(audio_path).decode()
         
         config = speech.RecognitionAudio(
             encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
